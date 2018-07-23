91cc80d9e494b8ff28b6db56232aabf8fe61386c
teiron <teiron.teiron+git@gmail.com>
2018-07-24 07:17:58 +0900

    724

--- form_plan_part.cs
+++ form_plan_part.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 
 partial class form{
+    byte f = 0;
     //メソッド supply 母港画面からの補給
     //引数 b :4bitでそれぞれの艦隊を選択する
     //        0b0000 で 1234 の順番
@@ -68,6 +69,7 @@ partial class form{
             return;
     }
 
+    //1-1出撃
     void Fielde1_1(){
         //delegate 母港画面に戻る
         Action home_port_return = () =>{
@@ -75,12 +77,24 @@ partial class form{
         };
 
         //動作開始
-        //母港から出撃画面に遷移
+        //母港画面確認
         a_non_b_click("母港_出撃", "母港_母港"); if(stop_flg)return;
+        //母港から海域選択画面に遷移
         a_non_b_click("出撃_出撃", "母港_出撃"); if(stop_flg)return;
         a_non_b_click("出撃海域_1", "出撃_出撃"); if(stop_flg)return;
         a_non_b_click("出撃決定_海域決定", "出撃海域詳細_1"); if(stop_flg)return;
         a_non_b_click("出撃決定_出撃決定", "出撃決定_海域決定"); if(stop_flg)return;
+        a_b_change_c_click("出撃決定_比較場所1","出撃決定_比較場所2","出撃決定_出撃決定");if(stop_flg)return;
 
+        //海域戦闘
+        do{
+            System.Threading.Thread.Sleep(1000)
+            a_click("母港_母港");
+            if(pic_con("戦闘_進撃"))a_click("戦闘_進撃");
+            if(pic_con("戦闘_夜戦"))a_click("戦闘_夜戦");
+            if(stop_flg)return;
+
+        }while(pic_con("母港_出撃"))
+        f &= 8;
     }
 }
